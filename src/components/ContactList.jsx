4eed180f3af37281f26940367b6eb4f0d7408797
DaVoronov <danulovoronov@gmail.com>
2023-08-22 11:45:52 +0300

    commit

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,11 +1,32 @@
+import { motion } from "framer-motion";
+
 const ContactList = ({ contacts, deleteContact }) => {
   return (
     <ul>
       {contacts.map((contact) => (
-        <li key={contact.id}>
+        <motion.li
+          initial={{
+            opacity: 0,
+            translateX: "100%",
+          }}
+          animate={{
+            opacity: 1,
+            translateX: 0,
+          }}
+          exit={{
+            opacity: 0,
+            translateX: "-100%",
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+          key={contact.id}
+        >
           {contact.name}: {contact.number}
-          <button onClick={() => deleteContact(contact.id)}>Delete</button>
-        </li>
+          <button type="button" onClick={() => deleteContact(contact.id)}>
+            Delete
+          </button>
+        </motion.li>
       ))}
     </ul>
   );
